fc516af29dc75f11bb20cec662d1f6977bb0b062
Khushi-Pathak-25 <khushipathak.2503@gmail.com>
2026-02-22 16:23:14 +0530

    Added QuantityMeasurementApp with Feet class and equals() implementation

---
+++ src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
@@ -0,0 +1,36 @@
+package com.apps.quantitymeasurement;
+
+public class QuantityMeasurementApp 
+{
+    public static class Feet{
+    	private final double value;
+    	
+    	public Feet(double value) {
+    		this.value = value;
+    	}
+    	
+    	@Override
+    	public boolean equals(Object obj) {
+    		// Reflexive check
+    		if(this == obj) {
+    			return true;
+    		}
+    		
+    		// Null & type check
+    		if(obj == null || this.getClass() != obj.getClass()) {
+    			return false;
+    		}
+    		
+    		// Use Double.compare for floating-point comparison
+    		Feet other = (Feet) obj;
+    		return Double.compare(this.value, other.value) == 0;
+    	}
+    }
+    
+    public static void main(String[]args) {
+    	Feet feet1 = new Feet(1.0);
+    	Feet feet2 = new Feet(1.0);
+    	
+    	System.out.println("Are they equal ? "+ feet1.equals(feet2));
+    }
+}
